1d8eed61df65ee81a7700217e2a5c6c2f7e21a9a
TranTLy <tranly237@gmail.com>
2019-11-29 12:23:01 +0700

    delete intention error in homepage

--- src/page/homepage/homepage.component.jsx
+++ src/page/homepage/homepage.component.jsx
@@ -4,7 +4,7 @@ import Directory from "../../components/directory/directory.component";
 import { HomePageContainer } from "./homepage.style";
 
 export const HomePage = () => {
-  return new Error("This is error to test Error boundary");
+  // return new Error("This is error to test Error boundary");
   return (
     <HomePageContainer>
       <Directory />
